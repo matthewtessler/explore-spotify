963ac5e9d5cc33a6f090ad6ebb4da0e450f98c5c
matthewtessler <matthewtessler96@gmail.com>
2017-08-29 12:48:56 -0400

    install babel and react

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-alert('hello world!');
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Explore Spotify
+      </div>
+    )
+  }
+}
+
+const root = document.querySelector('#app')
+ReactDOM.render(<App />, root)
--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,21 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, './public', './js')
+  },
+  module: {
+    rules: [
+       {
+         test: /.js$/,
+         exclude: /node_modules/,
+         use: [
+           {
+             loader: 'babel-loader',
+             options: {
+               cacheDirectory: true,
+             },
+           },
+         ],
+       },
+     ],
   }
 }
